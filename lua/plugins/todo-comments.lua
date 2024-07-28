37389efc7eb06a6c023173ff958f8f2b2c90ad34
Sean Wall <spw@amazon.com>
2024-07-27 23:13:11 -0400

    feat: add todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,5 +1,14 @@
--- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = true } },
+  'folke/todo-comments.nvim',
+  cmd = { 'TodoTrouble', 'TodoTelescope' },
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+    { "<leader>cxt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+    { "<leader>cxT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+    { "<leader>tx", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    { "<leader>tX", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+  },
 }
--- vim: ts=2 sts=2 sw=2 et
