04b590e8a56aa69aa0abacc95e4002b5689e3cd6
Sean Wall <spw@amazon.com>
2024-08-02 13:49:21 -0400

    fix: move trouble key maps to the top level

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -6,9 +6,10 @@ return {
   keys = {
     { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
     { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-    { "<leader>cxt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
-    { "<leader>cxT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-    { "<leader>tx", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-    { "<leader>tX", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+    { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+    { "<leader>xtx", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    { "<leader>xtX", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
   },
+  config = true,
 }
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -4,32 +4,32 @@ return {
   cmd = 'Trouble',
   keys = {
     {
-      '<leader>cxx',
+      '<leader>xx',
       '<cmd>Trouble diagnostics toggle<cr>',
       desc = 'Diagnostics (Trouble)',
     },
     {
-      '<leader>cxX',
+      '<leader>xX',
       '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
       desc = 'Buffer Diagnostics (Trouble)',
     },
     {
-      '<leader>cxs',
+      '<leader>xs',
       '<cmd>Trouble symbols toggle focus=false<cr>',
       desc = 'Symbols (Trouble)',
     },
     {
-      '<leader>cxl',
+      '<leader>xl',
       '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
       desc = 'LSP Definitions / references / ... (Trouble)',
     },
     {
-      '<leader>cxL',
+      '<leader>xL',
       '<cmd>Trouble loclist toggle<cr>',
       desc = 'Location List (Trouble)',
     },
     {
-      '<leader>cxQ',
+      '<leader>xQ',
       '<cmd>Trouble qflist toggle<cr>',
       desc = 'Quickfix List (Trouble)',
     },
